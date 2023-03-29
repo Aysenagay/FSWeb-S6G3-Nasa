e13b13e5a6ccb1d3a93d901aeef4f5576d8c6ec1
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-03-29 16:29:51 +0300

    app.js yapıldı.

--- src/nasa/Baslik.js
+++ src/nasa/Baslik.js
@@ -1,3 +1,7 @@
 import React from "react";
 import BuyukBaslik from "./altbolumler/BuyukBaslik.js";
 import Tarih from "./altbolumler/Tarih.js";
+
+export default function() {
+  return <div className="baslik"></div>;
+}
