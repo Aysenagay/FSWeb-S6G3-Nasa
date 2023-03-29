fd9f8dbf807b0fe33d8835db90c6f4dc89eaa98b
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-03-29 16:29:03 +0300

    app.js yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import Baslik from "./nasa/Baslik.js";
+import Footer from "./nasa/Footer.js";
+import OrtaBolum from "./nasa/OrtaBolum.js";
+import { sahteVeri } from "../sahteVeri";
+import axios from "axious";
 function App() {
+  const [tarih, setTarih] = useState("1998-03-28");
+  const [data, setData] = useState(sahteVeri);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=3wkI5lZxyiVojEhKgnaeG2kJwgzf00RAFI97pnsz"
+      )
+      .then((response) => {
+        setData(response.data);
+      });
+  }, [tarih]);
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Baslik />
+      <OrtaBolum />
+      <Footer />
     </div>
   );
 }
---
+++ src/nasa/Baslik.js
@@ -0,0 +1,3 @@
+import React from "react";
+import BuyukBaslik from "./altbolumler/BuyukBaslik.js";
+import Tarih from "./altbolumler/Tarih.js";
